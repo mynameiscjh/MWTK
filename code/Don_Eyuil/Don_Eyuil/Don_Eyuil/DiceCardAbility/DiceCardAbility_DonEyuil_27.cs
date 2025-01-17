879546c07035219c4ca74cd79c507f876a5ead3a
..D <997202198@qq.com>
2025-01-17 23:34:45 +0800

    实现了百般武艺第6和第7式

---
+++ code/Don_Eyuil/Don_Eyuil/Don_Eyuil/DiceCardAbility/DiceCardAbility_DonEyuil_27.cs
@@ -0,0 +1,22 @@
+﻿namespace Don_Eyuil.Don_Eyuil.DiceCardAbility
+{
+    public class DiceCardAbility_DonEyuil_27 : DiceCardAbilityBase
+    {
+        public static string Desc = "[命中时]消耗目标3层[流血]层流血并使本骰子重复投掷1次(至多4次)";
+        int count = 0;
+        public override void OnSucceedAttack(BattleUnitModel target)
+        {
+            if (!target.bufListDetail.HasBuf<BattleUnitBuf_bleeding>())
+            {
+                return;
+            }
+            var buf = target.bufListDetail.GetActivatedBufList().Find(x => x is BattleUnitBuf_bleeding) as BattleUnitBuf_bleeding;
+            if (buf.stack >= 3 && count <= 4)
+            {
+                buf.stack -= 3;
+                count++;
+                behavior.isBonusAttack = true;
+            }
+        }
+    }
+}
---
+++ code/Don_Eyuil/Don_Eyuil/Don_Eyuil/DiceCardSelfAbility/DiceCardSelfAbility_DmgCount.cs
@@ -0,0 +1,12 @@
+﻿namespace Don_Eyuil.Don_Eyuil.DiceCardSelfAbility
+{
+    public class DiceCardSelfAbility_DmgCount : DiceCardSelfAbilityBase
+    {
+        public static string Desc = "这是用来给第三骰记录击中次数的书页效果";
+        public int count = 0;
+        public override void OnSucceedAttack()
+        {
+            count++;
+        }
+    }
+}
