614d6098d5b7b5b8fdc7bdf43f0f6a679210293e
TkTian <orange1266@qq.com>
2025-01-26 10:21:30 +0800

    完成一部分xml

--- code/Don_Eyuil/Don_Eyuil/A_ReadMe.cs
+++ code/Don_Eyuil/Don_Eyuil/A_ReadMe.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Don_Eyuil.DnspyUserReadMe
+namespace A_DnspyUserReadMe
 {
-    public static class A_ReadMe
+    public class A_ReadMe
     {
-        public static string Note = "If you're using a decompiler like DnSpy to view this text, " +
-            "please consider checking out the open-source code for this project on GitHub:https://github.com/mynameiscjh/MWTK/tree/main " +
-            "You'll find more detailed comments and more sensible (non-decompiled) syntax there. <3";
+        public static string Note = "If you're using a decompiler like DnSpy to view this text,";
+        public static string Note2 = "please consider checking out the open-source code for this project on GitHub:https://github.com/mynameiscjh/MWTK/tree/main";
+        public static string Note3 = "You'll find more detailed comments and more sensible (non-decompiled) syntax there.";
+        public static string Heart = "<3";
+
+        A_ReadMe(string You,ref string Me,ref string YourUnderstandingDifficulty)
+        {
+            string Github = "https://github.com/mynameiscjh/MWTK/tree/main";
+            Me = You != Github ? "Sad" : "Happy";
+            YourUnderstandingDifficulty = You == Github ? "Easy" : "Hard";
+        }
     }
 }
