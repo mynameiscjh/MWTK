2a6e455887ab348f3c2096b4d305ec3c738622d6
..D <997202198@qq.com>
2025-02-03 13:20:32 +0800

    修改了SanSora_11的实现

--- code/Don_Eyuil/Don_Eyuil/San_Sora/Player/DiceCardSelfAbility/DiceCardSelfAbility_SanSora_11.cs
+++ code/Don_Eyuil/Don_Eyuil/San_Sora/Player/DiceCardSelfAbility/DiceCardSelfAbility_SanSora_11.cs
@@ -1,27 +1,16 @@
 ﻿using Don_Eyuil.San_Sora.Player.Buff;
+using System;
 
 namespace Don_Eyuil.San_Sora.Player.DiceCardSelfAbility
 {
     public class DiceCardSelfAbility_SanSora_11 : DiceCardSelfAbilityBase
     {
         public static string Desc = "若本书页应用在[血弓]骰子上则使本书页第二颗骰子最小值+2 [使用时]消耗至多9层[血羽]每消耗3层便使本书页所有骰子最大值+1";
-        int num = 0;
 
-        public override void OnUseCard()
+        public override void OnStartParrying()
         {
-            num = 0;
-            if (BattleUnitBuf_Don_Eyuil.UseBuf<BattleUnitBuf_Feather>(owner, 3))
-            {
-                num++;
-            }
-            if (BattleUnitBuf_Don_Eyuil.UseBuf<BattleUnitBuf_Feather>(owner, 3))
-            {
-                num++;
-            }
-            if (BattleUnitBuf_Don_Eyuil.UseBuf<BattleUnitBuf_Feather>(owner, 3))
-            {
-                num++;
-            }
+            card.ApplyDiceStatBonus(DiceMatch.AllDice, new DiceStatBonus() { max = Math.Min(BattleUnitBuf_Don_Eyuil.GetBufStack<BattleUnitBuf_Feather>(owner) / 3, 3) });
+            BattleUnitBuf_Don_Eyuil.GainBuf<BattleUnitBuf_Feather>(owner, -9);
         }
 
         public override void BeforeRollDice(BattleDiceBehavior behavior)
@@ -31,8 +20,6 @@ namespace Don_Eyuil.San_Sora.Player.DiceCardSelfAbility
             {
                 behavior.ApplyDiceStatBonus(new DiceStatBonus() { min = 2 });
             }
-
-            behavior.ApplyDiceStatBonus(new DiceStatBonus() { max = num });
 
         }
     }
